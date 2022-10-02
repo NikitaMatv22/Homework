dc822653d72923755e21f55127ea38c5a1901f98
NikitaMatv22 <nikimat238@gmail.com>
2022-10-02 23:38:16 +0300

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 31;
+            int b = 54;
+            int c = 11;
+            int odd = (a % 3 % 2 + b % 3 % 2 + c % 3 % 2)/2;
+
+            Console.WriteLine($" Результат {odd}");
+
+            ;
+        }
+    }
+}
